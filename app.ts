4412baf6796b4ccccd9f6ebea4148af93b6c89d2
Michael Werner <michael.werner@windowslive.com>
2015-06-15 06:01:26 -0700

    First try with Angular2

---
+++ app.ts
@@ -0,0 +1,20 @@
+/// <reference path="typings/angular2/angular2.d.ts" />
+
+import {Component, View, bootstrap} from 'angular2/angular2'
+
+@Component({
+    selector: 'my-app'
+})
+@View({
+    template: '<h1>Hello {{ name }}</h1>'
+})
+
+class MyAppComponent {
+    name: string;
+
+    constructor() {
+        this.name = 'Michael';
+    }
+}
+
+bootstrap(MyAppComponent);
